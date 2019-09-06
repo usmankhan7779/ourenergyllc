d80b08649f8eb39ac8ddfb4362a06768c82d74f7
shahidaslam0313 <shahid.aslam@brainplow.com>
2019-09-06 17:44:40 +0500

    asdfasdf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { 
-
+    // var list = document.getElementsByClassName("router__outlet")[0];
+    // list.getElementsByTagName("div")[0].classList.add("pd-tp")
   }
 
   myFunction = function () {
