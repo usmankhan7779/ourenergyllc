70be146946d043319779e16e79938462fcde0f35
Muhammad Usman Khan <usman.khan7779@gmail.com>
2019-09-12 17:56:12 +0500

    fdf

--- src/app/Container/Customer-Dashboard/offers/offers.component.ts
+++ src/app/Container/Customer-Dashboard/offers/offers.component.ts
@@ -5,6 +5,12 @@ import Swal from 'sweetalert2';
 import { MatPaginator, MatTableDataSource } from '@angular/material'
 import { Subscription } from 'rxjs/Subscription';
 import { ServerSocketService } from '../header/server-socket.service';
+// import { EnrollmentService } from '../Store/Services/enroll.service';
+import { Router } from '@angular/router';
+// import { MainService, PromoCodeService } from '../../Container/Store/Services';
+import { EnrollmentService } from '../../Store/Services/enroll.service';
+import { PromoCodeService } from '../../Store/Services';
+ 
 
 @Component({
     selector: 'app-offers',
@@ -16,17 +22,21 @@ export class OffersComponent implements OnInit {
     showSpinner1
     x
     y
+    promoCode = '';
     newOffer
     OfferProduct
+    products;
     error
-    selectProductBtnDisabled
+    ZipCode = localStorage.getItem('zip')
+    // selectProductBtnDisabled
     showMessageResponse: boolean
     public socketSubscription: Subscription
     @ViewChild(MatPaginator) paginator: MatPaginator
     dataSource = new MatTableDataSource()
     paymentColumns = ['pn', 'ad', 't1', 't2', 't3', 'tf']
     myHeaders: any = new HttpHeaders().set('content-type', 'application/json')
-    constructor(private socket: ServerSocketService, private http: HttpClient) { }
+    router: Router
+    constructor(private socket: ServerSocketService,_router: Router,private promos: PromoCodeService, private http: HttpClient,private enrollment: EnrollmentService) { }
 
     ngOnInit() {
         window.scrollTo(0, 0)
@@ -126,4 +136,87 @@ export class OffersComponent implements OnInit {
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator
     }
+    view_result(){
+        this.promos.uaCheck().subscribe(res => {
+          if (res['message']['bypass'] == true && res['message']['ua'] == false) {
+            localStorage.setItem('ua', "False")
+          }
+          else localStorage.setItem('ua', "True")
+        })
+        // if (this.promoCode == null) { this.promoCode = "" }
+        let data = {
+          zip_code: this.ZipCode,
+          promo_code: "" + this.promoCode,
+          client: "WattGenie-Web"
+        }
+        this.promos.searchPlan(data).subscribe(res => {
+          console.log(res)
+          // localStorage.removeItem('duns')
+          // if (res["status"] == false) {
+          //   this.showError = true
+          //   this.error = res["Error"]
+          //   this.submitBtnDisabled = false
+          // }
+          // if (res["status"] == true) {
+          //   // this.showSpinner = false
+          //   // this.showError = false
+          //   // this.submitBtnDisabled = false
+          //   if (res["tdsp_status"] == false) {
+          //     localStorage.setItem('duns', res['message'][0]['provider_id'])
+          //     this.showFilteredProducts = true
+          //     this.showPlans = true
+              this.products = res["message"]
+          //     localStorage.setItem('promotionCode', JSON.stringify(res['promo_code']))
+          //     localStorage.removeItem('zip')
+          //     this.submitBtnDisabled = false
+          //   }
+          //   else {
+          //     this.showTdsp = true
+          //     this.tdsps = res['message']['row']
+          //     this.submitBtnDisabled = false
+          //   }
+          // }
+          // else if (res["status"] == false) {
+          //   this.showFilteredProducts = false
+          //   this.showSpinner = false
+          //   this.showError = true
+          //   this.showPlans = false
+          //   this.error = res["message"]
+          //   localStorage.removeItem('zip')
+          //   this.submitBtnDisabled = false
+          // }
+        })
+      
+      }
+    selectProductBtnDisabled: boolean[] = []
+  enroll(i) {
+    this.selectProductBtnDisabled[i] = true
+    let data = {
+      product_pk: this.newOffer[i].id,
+      enroll_product: this.newOffer[i].product_id,
+      rate: this.newOffer[i].rate,
+      batch_rate: this.newOffer[i].batch_rate,
+      contract_term: this.newOffer[i].term,
+    }
+    this.enrollment.sendProductDataForSession(data).subscribe(res => {
+      if (res['status'] == true) {
+        this.selectProductBtnDisabled[i] = false
+        localStorage.setItem('zip', this.ZipCode)
+        this.router.navigate(['/enroll'])
+
+        localStorage.setItem('productSummary', JSON.stringify(this.newOffer[i]))
+      }
+      if (res["status"] == false && res["redirect_url"] != null && res["redirect_url"] != undefined && res["redirect_url"] != '') {
+        Swal('Your session has expired. Please refresh the page and try again', '', 'error').then(() => {
+          this.selectProductBtnDisabled[i] = false
+          this.ngOnInit()
+        })
+      }
+      else {
+        this.selectProductBtnDisabled[i] = false
+      }
+    }, () => {
+      this.selectProductBtnDisabled[i] = false
+    })
+  }
 }
