3cbe60e1b9a7da8ef45bee34bbd8a661364eb9cb
Muhammad Usman Khan <usman.khan7779@gmail.com>
2019-09-25 19:01:41 +0500

    asdada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // url: 'https://apis.wattcrm.com/',
-  url:'https://devbackend.wattcrm.com/',
+  url: 'https://apis.wattcrm.com/',
+  // url:'https://devbackend.wattcrm.com/',
   webenrollurl: 'https://webenrollapi.smartixai.com/',
 
   // webenrollurl: 'https://devwebenrollapi.smartixai.com/',
