f93104084f39210958430e93ff9f376a999055f6
Muhammad Usman Khan <usman.khan7779@gmail.com>
2019-10-03 09:53:25 +0500

    yjjghgjgt

--- src/app/single-blog-view/single-blog-view.component.ts
+++ src/app/single-blog-view/single-blog-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-single-blog-view',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBlogViewComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private route: ActivatedRoute,private http: Http) { }
+  private sub: any;
+  CatName;
+  data:any=[];
   ngOnInit() {
-  }
+    this.sub = this.route
+        .queryParams
+        .subscribe(params => {
+          // Defaults to 0 if no query param provided.
+          this.CatName = params['ProID'] || '0';
+        })
+        // alert(this.CatName)
+        this.profile();
 
+  }
+  profile() {
+
+    const headers = new Headers();
+ 
+    headers.append('Content-Type', 'application/json');
+    this.http.get('https://devbackend.wattcrm.com/portal/gettingblog_html/' +this.CatName +'/')
+      .subscribe(Res => {
+        this.data = Res.json();
+        console.log(this.data)
+    //     // this.content= this.data.content1
+    //     this.content=Res.json()[0]['content1'];
+    //  this.con=(this.content).replace(/<[^>]+>/gm, '')
+    //  $('#myDiv').html(data.);
+    //     console.log(this.content);
+        
+      });
+
+  }
 }
--- src/app/view-blog/view-blog.component.ts
+++ src/app/view-blog/view-blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-view-blog',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBlogComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private http: Http) { }
 
+  data:any=[];
   ngOnInit() {
+    this.profile();
   }
+  profile() {
 
+    const headers = new Headers();
+ 
+    headers.append('Content-Type', 'application/json');
+    this.http.get('https://devbackend.wattcrm.com/portal/gettingblog_all/')
+      .subscribe(Res => {
+        this.data = Res.json();
+        console.log(this.data)
+    //     // this.content= this.data.content1
+    //     this.content=Res.json()[0]['content1'];
+    //  this.con=(this.content).replace(/<[^>]+>/gm, '')
+    //     // $('#myDiv').html(this.content);
+    //     console.log(this.content);
+        
+      });
+
+  }
 }
