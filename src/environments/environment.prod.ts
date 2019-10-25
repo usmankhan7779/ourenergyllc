5141b4f1f2f6cc8ebc0ec8cd49ef0bef92d4adba
muhammadusmankhan <usman.khan7779@yahoo.com>
2019-10-25 10:26:19 +0500

    url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  // url: 'https://apis.wattcrm.com/',
-  url:'https://devbackend.wattcrm.com/',
-  // webenrollurl: 'https://webenrollapi.smartixai.com/',
+  url: 'https://apis.wattcrm.com/',
+  // url:'https://devbackend.wattcrm.com/',
+  webenrollurl: 'https://webenrollapi.smartixai.com/',
 
-  webenrollurl: 'https://devwebenrollapi.smartixai.com/',
+  // webenrollurl: 'https://devwebenrollapi.smartixai.com/',
   
   backendurl: 'https://backend.wattcrm.com/',
   agenturl: 'https://agentapis.wattcrm.com/',
